e659d84535432aae524c83cc0bbd39140e07f128
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-08-08 00:54:41 +0500

    Notification Component Completed

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Notification = () => {
   return (
-    <div>Notification</div>
-  )
-}
+    <div className=" h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer">
+      Free delivery for all orders over $50. Order your food now!
+    </div>
+  );
+};
 
-export default Notification
+export default Notification;
