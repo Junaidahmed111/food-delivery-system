ff3263a15264c0341e44f962e019fcad73591700
Junaidahmed111 <ahmedjuniad30@gmail.com>
2023-08-08 15:45:07 +0500

    Footer Completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
- import React from 'react'
- 
- const Footer = () => {
-   return (
-     <div>Footer</div>
-   )
- }
- 
- export default Footer
+import Link from "next/link";
+import React from "react";
+
+const Footer = () => {
+  return (
+  <div className="h-12 md:h-24 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between" >
+    <Link href='/' className="font-bold text-xl">DELCO</Link>
+    <p>ALL RIGHTS RESERVED</p>
+  </div>
+  );
+};
+
+export default Footer;
